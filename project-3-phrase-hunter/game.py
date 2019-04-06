eca6c3478a7f5070b31f1b80dd7b9d0a7c77f596
James Pestrak <james.w.pestrak@gmail.com>
2019-04-06 16:43:08 -0700

    Remove files from git tracking.

--- project-3-phrase-hunter/game.py
+++
@@ -1,7 +0,0 @@
-class Game(object):
-    """Class that contains methods for:
-    starting the game,
-    handling interactions,
-    getting a random phrase,
-    checking for a win/loss state,
-    removing 'lives' from a player."""
